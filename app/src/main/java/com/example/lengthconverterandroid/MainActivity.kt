568c6e03fba5f6652d09b0bd48de0df13d3b5a4b
toainguyen1504 <toaincgcss200874@fpt.edu.vn>
2025-09-22 22:35:38 +0700

    handle logic unit converter

--- app/src/main/java/com/example/lengthconverterandroid/MainActivity.kt
+++ app/src/main/java/com/example/lengthconverterandroid/MainActivity.kt
@@ -1,20 +1,88 @@
 package com.example.lengthconverterandroid
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.PopupMenu
+import androidx.constraintlayout.widget.ConstraintLayout
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val unitSelectTop  = findViewById<ConstraintLayout>(R.id.unitSelectTop)
+        val unitTopTxt   = findViewById<TextView>(R.id.unitTopTxt)
+
+        val unitSelectBottom  = findViewById<ConstraintLayout>(R.id.unitSelectBottom)
+        val unitBottomTxt   = findViewById<TextView>(R.id.unitBottomTxt)
+
+        val inputTop = findViewById<EditText>(R.id.inputTop)
+        val inputBottom = findViewById<EditText>(R.id.inputBottom)
+        val convertBtn = findViewById<ImageButton>(R.id.convertBtn)
+
+        setupSelect(unitSelectTop, unitTopTxt)
+        setupSelect(unitSelectBottom, unitBottomTxt)
+
+        // handle convert btn
+        convertBtn.setOnClickListener {
+            val inputStr = inputTop.text.toString()
+
+            if (inputStr.isEmpty()) {
+                Toast.makeText(this, "Please enter value!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val fromUnit = unitTopTxt.text.toString()
+            val toUnit = unitBottomTxt.text.toString()
+
+            val inputValue = inputStr.toDouble()
+
+
+            // convert back to meters
+            val inMetre = when (fromUnit) {
+                "Metre (m)" -> inputValue
+                "Millimetre (mm)" -> inputValue / 1000
+                "Mile (dặm)" -> inputValue * 1609.34
+                "Foot (ft)" -> inputValue * 0.3048
+                else -> inputValue
+            }
+
+            // convert from meters to target unit
+            val result = when (toUnit) {
+                "Metre (m)" -> inMetre
+                "Millimetre (mm)" -> inMetre * 1000
+                "Mile (dặm)" -> inMetre / 1609.34
+                "Foot (ft)" -> inMetre / 0.3048
+                else -> inMetre
+            }
+
+            // Format result
+            val df = DecimalFormat("#.##")
+            val resultStr = df.format(result)
+
+            inputBottom.setText(resultStr)
+
+        }
+
+    }
+
+    private fun setupSelect(clickView: ConstraintLayout, targetText: TextView) {
+        clickView.setOnClickListener { view ->
+            val popup = PopupMenu(this, view)
+            popup.menuInflater.inflate(R.menu.unit_menu, popup.menu)
+
+            popup.setOnMenuItemClickListener { item ->
+                targetText.text = item.title
+                true
+            }
+
+            popup.show()
+
         }
     }
 }
